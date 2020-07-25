2669b06424286b6379f3e64dfb84f9ed1904636f
Alex Root <newrou@mail.ru>
2020-07-25 18:52:07 +0700

    Dock script for Full peptides

--- Docking/dock-vina-all.sh
+++ Docking/dock-vina-all.sh
@@ -1,9 +1,14 @@
 
 for target in *-v[0-9][0-9].pdbqt
 do
-    echo $target" ~ "EGLNRPSG_1.pdbqt
-    ./dock-vina.sh $target EGLNRPSG_1.pdbqt
-    echo $target" ~ "YDPEYRNFW-1.pdbqt
-    ./dock-vina.sh $target YDPEYRNFW-1.pdbqt
+    echo $target" ~ "EGLNRPSGGCG-v1.pdbqt
+    ./dock-vina.sh $target EGLNRPSGGCG-v1.pdbqt
+    echo $target" ~ "YDPEYRNFWGCG-v1.pdbqt
+    ./dock-vina.sh $target YDPEYRNFWGCG-v1.pdbqt
+
+#    echo $target" ~ "EGLNRPSG-v1.pdbqt
+#    ./dock-vina.sh $target EGLNRPSG-v1.pdbqt
+#    echo $target" ~ "YDPEYRNFW-v1.pdbqt
+#    ./dock-vina.sh $target YDPEYRNFW-v1.pdbqt
 done
 
